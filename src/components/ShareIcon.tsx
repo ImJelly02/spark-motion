ed2f37fbc5719d51ca3727806eab2d3bb4a3f5f5
ImJelly02 <syeo0036@student.monash.edu>
2026-04-17 00:04:43 +0800

    Fix looping issue

--- src/components/ShareIcon.tsx
+++ src/components/ShareIcon.tsx
@@ -3,6 +3,7 @@ import { motion, useAnimation } from 'motion/react'
 import { iconDefaults } from '../types/icon'
 import type { SparkMotionIconProps } from '../types/icon'
 import {
+  getAnimationConfig,
   getIconInteractionStyle,
   getWhileTapConfig,
   isMountTriggeredAnimation,
@@ -21,29 +22,21 @@ export function ShareIcon(props: SparkMotionIconProps) {
     onClick,
   } = props
 
-  const circleControls = useAnimation()
+  const controls = useAnimation()
   const whileTap = getWhileTapConfig(animation)
   const isInteractive = Boolean(onClick || (animated && !isMountTriggeredAnimation(animation)))
-
-  const resolvedDuration = duration ?? 0.6
-
-  const triggerCircles = () => {
-    void circleControls.start({
-      r: [3, 3.8, 3],
-      transition: { duration: resolvedDuration },
-    })
-  }
 
   useEffect(() => {
     if (!animated || (!isMountTriggeredAnimation(animation) && !loop)) {
       return
     }
-    triggerCircles()
-  }, [animated, animation, circleControls, duration, loop])
+
+    void controls.start(getAnimationConfig(animation, duration, loop))
+  }, [animated, animation, controls, duration, loop])
 
   const handleClick = () => {
     if (animated && !isMountTriggeredAnimation(animation)) {
-      triggerCircles()
+      void controls.start(getAnimationConfig(animation, duration, loop))
     }
     onClick?.()
   }
@@ -64,16 +57,15 @@ export function ShareIcon(props: SparkMotionIconProps) {
       onClick={handleClick}
       style={getIconInteractionStyle(isInteractive)}
     >
-      <g transform="translate(12 12) scale(1.12) translate(-12 -12)">
-        <motion.circle cx="18" cy="6" r="3" animate={circleControls} />
-        <motion.circle cx="6" cy="12" r="3" animate={circleControls} />
-        <motion.circle cx="18" cy="18" r="3" animate={circleControls} />
+      {/* Only this group animates */}
+      <motion.circle cx="18" cy="6" r="3" animate={controls}/>
+      <motion.circle cx="6" cy="12" r="3" animate={controls}/>
+      <motion.circle cx="18" cy="18" r="3" animate={controls}/>
 
-        {/* Lines stay static */}
-        <path d="M8.5 10.5c3-2 2-4 7-3.5" />
-        <path d="M8.5 13.5c3 2 2 4 7 3.5" />
-        <path d="M16.5 4.5a1.5 0 0 1 1.5 1" />
-      </g>
+      {/* Lines stay static */}
+      <path d="M8.5 10.5c3-2 2-4 7-3.5" />
+      <path d="M8.5 13.5c3 2 2 4 7 3.5" />
+      <path d="M16.5 4.5a1.5 0 0 1 1.5 1" />
     </motion.svg>
   )
 }
